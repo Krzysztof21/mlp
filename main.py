3f344c37248469ff21b5ea91bff51c50d0140d6d
student186 <k.cwi.tl@gmail.com>
2019-03-12 13:09:37 +0100

    sometinhs working, but no meaningful result

--- main.py
+++ main.py
@@ -3,24 +3,44 @@ import neuron
 import matplotlib.pyplot as plt
 import scipy.stats as sci
 import numpy as np
-
-#class Layer:
-
-    #def __init__(self, inp, n):
-
-
-
-
-
-#------------------------------------------------------------
-
-inp = [1,2,3,4]
-initweights = [0.5] * 4
-
-n = neuron.Neuron(inp, initweights)
-
 import dataLoader
 
-#plt.plot(inp, initweights)
-#plt.show()
 
+def sigmoid(x):
+    return 1.0/(1+ np.exp(-x))
+
+def sigmoid_derivative(x):
+    return x * (1.0 - x)
+
+class NeuralNetwork:
+    def __init__(self, x, y):
+        self.input      = x
+        self.weights1   = np.random.rand(4, self.input.shape[0])
+        self.weights2   = np.random.rand(self.input.shape[0],3)
+        self.y          = y
+        self.output     = np.zeros(self.y.shape)
+
+    def feedforward(self):
+        self.layer1 = sigmoid(np.dot(self.input, self.weights1))
+        self.output = sigmoid(np.dot(self.layer1, self.weights2))
+
+    def backprop(self):
+        # application of the chain rule to find derivative of the loss function with respect to weights2 and weights1
+        d_weights2 = np.dot(self.layer1.T, (2*(self.y - self.output) * sigmoid_derivative(self.output)))
+        d_weights1 = np.dot(self.input.T,  (np.dot(2*(self.y - self.output) * sigmoid_derivative(self.output), self.weights2.T) * sigmoid_derivative(self.layer1)))
+
+        # update the weights with the derivative (slope) of the loss function
+        self.weights1 += d_weights1
+        self.weights2 += d_weights2
+
+
+if __name__ == "__main__":
+    input = dataLoader.inp
+    output = dataLoader.output
+    nn = NeuralNetwork(input,output)
+
+    for i in range(1500):
+        nn.feedforward()
+        nn.backprop()
+
+print(nn.output)
