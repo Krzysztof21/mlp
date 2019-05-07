74095d6b582a738ce238c587120662af743e4883
Krzysztof Ćwieka <cwieka.krzysztof@gmail.com>
2019-05-07 19:55:21 +0200

    Main scaffold of a network ready

--- main.py
+++ main.py
@@ -1,15 +1,8 @@
 import math as m
-import neuron
+import mlp
 import matplotlib.pyplot as plt
 import scipy.stats as sci
 import numpy as np
-
-#class Layer:
-
-    #def __init__(self, inp, n):
-
-
-
 
 
 #------------------------------------------------------------
@@ -17,9 +10,28 @@ import numpy as np
 inp = [1,2,3,4]
 initweights = [0.5] * 4
 
-n = neuron.Neuron(inp, initweights)
+n = mlp.Neuron(initweights)
 
-import dataLoader
+lay1 = mlp.Layer(4,initweights)
+lay1.layerOutput(inp)
+print(lay1.out)
+
+lay2 = mlp.Layer(8, initweights)
+lay2.layerOutput(inp)
+print(lay2.out)
+
+net = mlp.Network(4,4)
+net.addLayer(lay1)
+net.addLayer(lay2)
+net.addInputLayer(lay2)
+weightsSix = [0.5]*8
+net.addArbLayer(8)
+
+inp2 = [1,2,3,4,3,12,4,5]
+print(net.Layers)
+print(net.getNetOutput(inp2))
+
+#import dataLoader
 
 #plt.plot(inp, initweights)
 #plt.show()
